91f71725de0c120bac71134994084ce3e116ed6d
nveljovic <nikolaveljokv@gmail.com>
2021-01-19 10:08:07 -0500

    status created

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const singupController = require('../controllers/user/singup');
 const singinController = require('../controllers/user/singin');
-
+const statusController = require('../controllers/user/status');
 
 router.post('/signup', singupController);
 router.post('/signin', singinController);
+router.get('/status', statusController);
 
 module.exports = router;
--- services/verifyUserAuth.js
+++ services/verifyUserAuth.js
@@ -5,42 +5,36 @@ const validator = require("validator");
 
 module.exports = verifyUserAuth = asyncHandler(async (req, res, next) => {
 
-    const csrf = req.body.csrf;
-    console.log("csrf" + csrf);
-    const token = await req.cookies["token"];
-    console.log("token" + token);
+    const csrf = req.headers['x-csrf-token'];
+    const token = req.cookies["token"];
+
     if(!(csrf && validator.isAlphanumeric(csrf) && token && validator.isJWT(token))) {
-        console.log("1");
         throw new ErrorHandler(401, "Unauthorized");
     }
 
     const decoded = await verifyAuthToken(token);
 
     if (!(decoded && decoded.exp && decoded.csrf && decoded.uid)) {
-        console.log("2");
         throw new ErrorHandler(401, "Unauthorized");
     }
 
     const date = Math.floor(Date.now() / 1000);
 
     if(decoded.exp < date) {
-        console.log("3");
         throw new ErrorHandler(401, "Unauthorized");
     }
 
     if(csrf !== decoded.csrf) {
-        console.log("4");
         throw new ErrorHandler(401, "Unauthorized");
     }
 
     if(!validator.isMongoId(decoded.uid)) {
-        console.log("5");
         throw new ErrorHandler(401, "Unauthorized");
     }
 
-    req.uid = decoded.uid;
-    req.date = date;
+    const mongoDate = Date.now();
 
-    console.log(req.uid, req.date);
+    req.uid = decoded.uid;
+    req.date = mongoDate;
     next();
 });
