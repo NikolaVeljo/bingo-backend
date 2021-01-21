7993793c6e5883cecea67b3b2bf6d3a8e0530b85
nveljovic <nikolaveljokv@gmail.com>
2021-01-21 16:03:12 -0500

    web socket removed

--- index.js
+++ index.js
@@ -12,77 +12,6 @@ const app = require("./app");
 // const httpsServer = https.createServer(credentials, app);
 const httpServer = http.createServer(app);
 
-const wss = new WebSocket.Server({
-    port: 8081,
-});
-
-let randomNumbersArr = [];
-let gameInterval;
-let pauseInterval;
-let pauseSeconds;
-
-const roundInterval = () => {
-    // Kreiramo next round i podesavamo variablu nextRound sa id-jem koji smo kreirali
-    //
-    randomNumbersArr = [];
-
-    console.log("roundInterval started");
-    gameInterval = setInterval(getRandomNumber, 10000)
-}
-
-//
-const gamePauseHandler = () => {
-
-    if ( pauseSeconds > 0 ){
-        console.log(pauseSeconds);
-        pauseSeconds -= 1;
-    } else {
-        console.log("pauseInterval clearde");
-        clearInterval(pauseInterval);
-        console.log( 'roundInterval started from pause handler ');
-        roundInterval();
-    }
-}
-
-const getRandomNumber = () => {
-    console.log( 'inside random number function')
-
-    if (randomNumbersArr.length < 6) {
-
-        let randomNumber = Math.floor(Math.random() * 80 ) + 1;
-
-        if ( randomNumbersArr.indexOf( randomNumber ) === -1 ) {
-
-            randomNumbersArr.push( randomNumber );
-            console.log( randomNumbersArr )
-        } else {
-            console.log(" inside random number else")
-            getRandomNumber();
-
-        }
-    } else {
-        console.log('clearInterval');
-        pauseSeconds = 20;
-
-        // Stavljamo next round da bude current round
-        //
-        clearInterval(gameInterval);
-        pauseInterval = setInterval(gamePauseHandler,1000);
-    }
-}
-
-wss.on('connection', function connection(ws) {
-
-    setInterval(function(){
-        ws.send(JSON.stringify({
-            pause: pauseSeconds,
-            numbers: randomNumbersArr
-        }));
-    }, 1000);
-
-});
-
-
 (async() => {
     try {
         await mongoose.connect(process.env.DATABASE, {
@@ -93,7 +22,6 @@ wss.on('connection', function connection(ws) {
         });
         httpServer.listen(8080);
         console.log("Server started");
-        roundInterval();
     } catch (e) {
         console.log(e)
     }
