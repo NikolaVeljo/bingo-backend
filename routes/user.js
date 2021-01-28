68d0d037901a293918af584fae6f144a365775a9
nveljovic <nikolaveljokv@gmail.com>
2021-01-28 14:01:31 -0500

    auth handler

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,9 @@ const profileController = require("../controllers/profile");
 
 router.post("/sign-up", authController.signUp);
 router.post("/sign-in", authController.signIn);
+
 router.get("/sign-out", authController.signOut);
 
-router.get("/profile", profileController.profile);
+router.get("/profile", authHandler, profileController.profile);
 
 module.exports = router;
