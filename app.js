d11d3fdd37d8bca271c91f697ed5650880e5e769
nveljovic <nikolaveljokv@gmail.com>
2021-01-29 10:49:21 -0500

    comm

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require("express");
 const userRouter = require("./routes/user");
 const sessionHandler = require("./middlewares/sessionHandler");
 const { errorHandler } = require("./middlewares/errorHandler");
+const cors = require('cors');
 
 const app = express();
 
 // if you run behind a proxy ( e.g. kubernates, ngix)
 // app.set('trust proxy', 1)
+app.use(
+    cors({
+        origin: [
+			process.env.NODE_ENV === "production" ? process.env.WEBSITE : "http://localhost:3000"
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(sessionHandler);
 
