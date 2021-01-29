79c8bf9877b9eabf5f9fb0e91ad103e0287fdd65
nveljovic <nikolaveljokv@gmail.com>
2021-01-29 09:49:41 -0500

    prod || dev

--- middlewares/sessionHandler.js
+++ middlewares/sessionHandler.js
@@ -4,12 +4,13 @@ const redisClient = require("../db/redis");
 
 const RedisStore = connectRedis(session);
 
-const sessionHandler = session({
+const sessionObject = {
 	store: new RedisStore({
 		client: redisClient,
 	}),
 	name: "session",
-	secret: "secret",
+	// domain: process.env.NODE_ENV === "production" ? process.env.COOKIE_DOMAIN : "",
+	secret: proccess.env.REDIS_SECRET,
 	saveUninitialized: false,
 	resave: false,
 	rolling: true,
@@ -18,6 +19,13 @@ const sessionHandler = session({
 		httpOnly: true,
 		maxAge: 1000 * 60 * 30,
 	},
-});
+};
+
+if (process.env.NODE_ENV === "production") {
+	app.set("trust proxy", 1); 
+	sessionObject.cookie.secure = true;
+}
+
+const sessionHandler = session(sessionObject);
 
 module.exports = sessionHandler;
