c8b2ea6a9a75f3b449596a57d248fd1e6dfea222
nveljovic <nikolaveljokv@gmail.com>
2021-02-03 17:13:07 -0500

    cc

--- app.js
+++ app.js
@@ -2,45 +2,44 @@ const express = require("express");
 const userRouter = require("./routes/user");
 const sessionHandler = require("./middlewares/sessionHandler");
 const { errorHandler } = require("./middlewares/errorHandler");
-const cors = require('cors');
-const path = require('path');
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-if ( process.env.NODE_ENV === 'production'){
-	app.set('trust proxy', 1);
+if (process.env.NODE_ENV === "production") {
+	app.set("trust proxy", 1);
 }
 
 app.use(
-    cors({
-        origin: [
-			process.env.NODE_ENV === "production" ? process.env.WEBSITE : 'http://localhost:3000',
-        ],
-        credentials: true,
-    })
+	cors({
+		origin: [
+			process.env.NODE_ENV === "production"
+				? process.env.WEBSITE
+				: "http://localhost:3000",
+		],
+		credentials: true,
+	})
 );
-
 
 app.use(express.json());
 app.use(sessionHandler);
 
 app.use("/api", userRouter);
 
-if ( process.env.NODE_ENV === "production" ){
-    console.log(__dirname)
-    app.use("", express.static(path.join(__dirname, 'build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+	console.log(__dirname);
+	app.use("", express.static("client/build"));
+	// Handle React routing, return all requests to React app
+	app.get("*", function (req, res) {
+		res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+	});
 
-    // app.use(express.static("client/build"));
-    // app.get('*', (req,res) => {
-    //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html") );
-    // });
+	// app.use(express.static("client/build"));
+	// app.get('*', (req,res) => {
+	//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html") );
+	// });
 }
-
-
 
 app.use((err, req, res, next) => {
 	errorHandler(err, res);
